4d5597c108051f7522d4eadc108a87dd8434c520
Richard Lock <r.j.lock@derby.ac.uk>
2018-06-12 01:14:46 +0100

    Added step to create docker daemon config directory

--- scripts/Install-Dotfiles.ps1
+++ scripts/Install-Dotfiles.ps1
@@ -1,9 +1,11 @@
 # Docker daemon config
-$dockerDaemonConfigFile = 'C:\ProgramData\docker\config\daemon.json'
+$dockerDaemonConfigDir = 'C:\ProgramData\docker\config'
+$dockerDaemonConfigFile = 'daemon.json'
 $dockerDaemonConfigUri = 'https://raw.githubusercontent.com/richardlock/dotfiles/windows/docker/daemon.json'
 
 # Enable TLS 1.2 for downloads
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
 # Install Docker daemon config
-Invoke-WebRequest -Uri $dockerDaemonConfigUri -OutFile $dockerDaemonConfigFile
+New-Item -path “C:\winpe_x86\mount” -type directory
+Invoke-WebRequest -Uri $dockerDaemonConfigUri -OutFile (Join-Path $dockerDaemonConfigDir $dockerDaemonConfigFile)
