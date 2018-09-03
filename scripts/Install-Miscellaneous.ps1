a251a0e3c8ca04419f2fe83440cf808e6c258919
richardlock <richardlock@users.noreply.github.com>
2018-09-03 23:29:44 +0100

    Removed font installation

--- scripts/Install-Miscellaneous.ps1
+++ scripts/Install-Miscellaneous.ps1
@@ -1,26 +1,9 @@
-# ConEmu font
-$fontFileName = 'Ubuntu Mono Nerd Font Complete Mono Windows Compatible.ttf'
-$fontUri = 'https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/UbuntuMono/Regular/complete/Ubuntu%20Mono%20Nerd%20Font%20Complete%20Mono%20Windows%20Compatible.ttf'
-$systemFontsPath = 'C:\Windows\Fonts'
-
 # Visual Studio Code extensions
 $vscodeExtensions = @(
   'peterjausovec.vscode-docker',
   'ms-vscode.powershell',
   'vscoss.vscode-ansible'  
 )
-
-# Install font for ConEmu
-$fontFilePath = Join-Path $env:temp $fontFileName
-$systemFontsFolder = (New-Object -ComObject Shell.Application).Namespace(0x14)
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-Invoke-WebRequest -Uri $fontUri -OutFile $fontFile
-If (!(Test-Path (Join-Path $systemFontsPath $fontFileName))) {
-  $systemFontsFolder.CopyHere($fontFilePath)
-}
-If (Test-Path $fontFilePath) {
-  Remove-Item $fontFilePath
-}
 
 # Install Visual Studio Code extensions
 $vscodeExtensions | ForEach-Object {
