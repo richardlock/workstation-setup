ad1486bbde6274a5cdd0c84ec453dbdb4d9d49a7
richardlock <richardlock@users.noreply.github.com>
2018-09-03 20:28:51 +0100

    Updated WSL install script

--- scripts/Install-WslUbuntu.ps1
+++ scripts/Install-WslUbuntu.ps1
@@ -1,13 +1,13 @@
-$wslUbuntuUri = 'https://aka.ms/wsl-ubuntu-1604'
+$wslUbuntuUri = 'https://aka.ms/wsl-ubuntu-1804'
 
 # Download Ubuntu archive
 If (!(Test-Path -Path '~\Ubuntu.zip')) {
-  Invoke-WebRequest -Uri $wslUbuntuUri -OutFile '~/Ubuntu.zip'
+  Invoke-WebRequest -Uri $wslUbuntuUri -OutFile '~/Ubuntu.zip' -UseBasicParsing
 }
   
 # Extract Ubuntu archive
 If (!(Test-Path -Path '~\Ubuntu')) {
-  Expand-Archive '~\Ubuntu.zip' '~\Ubuntu'
+  Expand-Archive -Path '~\Ubuntu.zip' -DestinationPath '~\Ubuntu'
 }
   
 # Initialise Ubuntu
