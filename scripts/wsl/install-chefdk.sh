251301f733fac4ff7ccfb28002d4b4f338ec1481
richardlock <richardlock@users.noreply.github.com>
2019-01-11 08:30:01 +0000

    Added kitchen-docker to chefdk install

--- scripts/wsl/install-chefdk.sh
+++ scripts/wsl/install-chefdk.sh
@@ -2,3 +2,6 @@
 curl -O https://packages.chef.io/files/stable/chefdk/3.5.13/ubuntu/18.04/chefdk_3.5.13-1_amd64.deb
 sudo dpkg -i chefdk_3.5.13-1_amd64.deb
 rm -f chefdk_3.5.13-1_amd64.deb
+
+# Install kitchen-docker
+chef gem install kitchen-docker
