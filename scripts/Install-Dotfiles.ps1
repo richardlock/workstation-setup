c88b1df1fabeadfe024a647194fdd15695c26953
richardlock <richardlock@users.noreply.github.com>
2019-11-11 09:44:37 +0000

    Disabled Windows dotfile steps
    Needs more testing

--- scripts/Install-Dotfiles.ps1
+++ scripts/Install-Dotfiles.ps1
@@ -13,8 +13,8 @@ $dockerDaemonConfigDir = "$env:ALLUSERSPROFILE\docker\config"
 git clone -b $dotfilesRepoBranch --single-branch $dotfilesRepoUri $dotfilesDir
 
 # ConEmu symbolic link
-New-Item -ItemType SymbolicLink -Path "$conEmuConfigDir\ConEmu.xml" -Value "$dotfilesDir\conemu\ConEmu.xml"
+# New-Item -ItemType SymbolicLink -Path "$conEmuConfigDir\ConEmu.xml" -Value "$dotfilesDir\conemu\ConEmu.xml"
 
 # Docker symbolic link
-New-Item -ItemType Directory -Path $dockerDaemonConfigDir
-New-Item -ItemType SymbolicLink -Path "$dockerDaemonConfigDir\daemon.json" -Value "$dotfilesDir\docker\daemon.json"
+# New-Item -ItemType Directory -Path $dockerDaemonConfigDir
+# New-Item -ItemType SymbolicLink -Path "$dockerDaemonConfigDir\daemon.json" -Value "$dotfilesDir\docker\daemon.json"
