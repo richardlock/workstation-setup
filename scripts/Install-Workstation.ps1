3cdcaa544f33cf750437130a54c0ed17ce9c779d
richardlock <richardlock@users.noreply.github.com>
2022-07-18 15:06:15 +0100

    Updated scripts

--- scripts/Install-Workstation.ps1
+++ scripts/Install-Workstation.ps1
@@ -5,29 +5,14 @@ $webClient = New-Object System.Net.WebClient
 # Enable Windows capabilities
 Invoke-Expression ($webClient.DownloadString('https://raw.githubusercontent.com/richardlock/workstation-setup/master/scripts/Enable-WindowsCapabilities.ps1'))
 
-# Enable Windows optional features
-Invoke-Expression ($webClient.DownloadString('https://raw.githubusercontent.com/richardlock/workstation-setup/master/scripts/Enable-WindowsOptionalFeatures.ps1'))
-
-# Install Chocolatey packages
-Invoke-Expression ($webClient.DownloadString('https://raw.githubusercontent.com/richardlock/workstation-setup/master/scripts/Install-ChocolateyPackages.ps1'))
+# Install Scoop packages
+Invoke-Expression ($webClient.DownloadString('https://raw.githubusercontent.com/richardlock/workstation-setup/master/scripts/Install-ScoopPackages.ps1'))
 
 # Install miscellaneous
 Invoke-Expression ($webClient.DownloadString('https://raw.githubusercontent.com/richardlock/workstation-setup/master/scripts/Install-Miscellaneous.ps1'))
-
-# Install dotfiles
-Invoke-Expression ($webClient.DownloadString('https://raw.githubusercontent.com/richardlock/workstation-setup/master/scripts/Install-Dotfiles.ps1'))
 
 # Install Windows Subsystem for Linux
 Invoke-Expression ($webClient.DownloadString('https://raw.githubusercontent.com/richardlock/workstation-setup/master/scripts/Install-WslUbuntu.ps1'))
 
 # Configure Windows Subsystem for Linux
 Start-Process -FilePath wsl -ArgumentList 'sh -c "$(curl -fsSL https://raw.githubusercontent.com/richardlock/workstation-setup/master/scripts/wsl/configure-wsl.sh)"' -Wait
-
-# Install Ansible in Windows Subsystem for Linux
-Start-Process -FilePath wsl -ArgumentList 'sh -c "$(curl -fsSL https://raw.githubusercontent.com/richardlock/workstation-setup/master/scripts/wsl/install-ansible.sh)"' -Wait
-
-# Install Docker in Windows Subsystem for Linux
-Start-Process -FilePath wsl -ArgumentList 'sh -c "$(curl -fsSL https://raw.githubusercontent.com/richardlock/workstation-setup/master/scripts/wsl/install-docker.sh)"' -Wait
-
-# Install ChefDK in Windows Subsystem for Linux
-Start-Process -FilePath wsl -ArgumentList 'sh -c "$(curl -fsSL https://raw.githubusercontent.com/richardlock/workstation-setup/master/scripts/wsl/install-chefdk.sh)"' -Wait
